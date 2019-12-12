b3cc138d85563884d5c765da752b8097928e0a2e
TomBenShmueli <tombenshmueli@gmail.com>
2019-12-12 14:37:08 +0200

    Updated project files + CLION files + cmake.

---
+++ cmake-build-debug/CMakeFiles/MMN13.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MMN13.dir/d_heap.c.o"
+  "MMN13.exe"
+  "MMN13.pdb"
+  "libMMN13.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MMN13.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
